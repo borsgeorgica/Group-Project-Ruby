34ecdef4fa629706843a90f7526fbf5f86f8f129
Georgica Bors <gbors1@sheffield.ac.uk>
2017-03-07 23:20:43 +0000

    Added login functionality

--- main.rb
+++ main.rb
@@ -2,14 +2,22 @@
 require 'sinatra'
 require_relative 'twitter.rb'
 
+enable :sessions
+set :session_secret, 'super secret'
+
 set :bind, '0.0.0.0' # Only needed if you're running from Codio
 
 get '/' do
+    redirect '/login' unless session[:logged_in]
+    erb :index
+ 
+end
 
+
+get '/orders' do  
     t = TwitterInteract.new()
     @list = t.get_tweets("@spicyslice #order") #keyword as paramater
     erb :display_tweets
-    
 end
 
 
@@ -23,4 +31,26 @@ end
 
 get '/login' do
      erb :login
+    
 end
+
+post '/login' do
+#     puts params[:password]
+#     puts params[:username]
+    if params[:password] == 'secret' and params[:username] == 'bors'
+        session[:logged_in] = true
+        session[:login_time] = Time.now
+        redirect '/index' 
+    end
+    
+     @error = "Password incorrect"
+     erb :login
+   
+end
+
+get '/logout' do
+    session.clear
+    redirect '/login'
+#     erb :logout
+end
+      
