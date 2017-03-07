fe058e24dc3e40546ca0cc6a3be7b7a9f85305e8
Georgica Bors <gbors1@sheffield.ac.uk>
2017-03-07 21:16:28 +0000

    Fix

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ set :bind, '0.0.0.0' # Only needed if you're running from Codio
 get '/' do
 
     t = TwitterInteract.new()
-    @list = t.get_tweets("George") #keyword as paramater
+    @list = t.get_tweets("@spicyslice #order") #keyword as paramater
     erb :display_tweets
     
 end
