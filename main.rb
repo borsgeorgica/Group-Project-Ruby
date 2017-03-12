d881d774766ae1fc7985dbbd524d76dc782c8083
Georgica Bors <gbors1@sheffield.ac.uk>
2017-03-12 15:35:07 +0000

    user is able to register now

--- main.rb
+++ main.rb
@@ -1,11 +1,21 @@
 
 require 'sinatra'
 require_relative 'twitter.rb'
+require 'sqlite3'
+
+include ERB::Util
+
 
 enable :sessions
 set :session_secret, 'super secret'
 
 set :bind, '0.0.0.0' # Only needed if you're running from Codio
+
+before do
+    @db = SQLite3::Database.new './database/database.sqlite'
+    
+end
+
 
 get '/' do
     redirect '/login' unless session[:logged_in]
@@ -29,9 +39,48 @@ get  '/index'  do
     erb :index
 end
 
+
+
 get '/register' do
+     @submitted = false
      erb :register
 end
+
+post '/register' do
+    @submitted = true
+    
+    # sanatize values
+    @username = params[:username].strip
+    @name = params[:name].strip
+    @surname = params[:surname].strip
+    @email = params[:email].strip
+    @password = params[:password].strip
+    @contact_number = params[:number].strip
+    @address = params[:address].strip
+    
+    # perform validation
+    @username_ok = !@username.nil? && @username !=""
+#     count = @db.get_first_value(
+#         'SELECT COUNT(*) FROM user WHERE email = ? AND number = ?',
+#         [@email,@contact_number])
+    @unique = true #(count == 0)
+    @all_ok = @username_ok && @unique
+    
+    # add data to the database
+    if @all_ok
+        
+        @db.execute(
+            'INSERT INTO user VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [@username, @name, @surname, @email, @password, @contact_number, @address])
+        redirect '/index'
+    end
+    
+    erb :register
+        
+    
+end
+
+
 
 get '/login' do
      erb :login
